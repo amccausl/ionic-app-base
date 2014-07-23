fb1160e79f6163e6096ad4e799c5d3d05802f52c
Alex McCausland <alex.mccausland@gmail.com>
2014-07-23 15:18:33 -0400

    Add gulpfile reload, templates in scripts stream [1h]

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,15 @@ var _           = require('underscore');
 var html2js     = require('gulp-ng-html2js');
 var jade        = require('gulp-jade');
 var jshint      = require('gulp-jshint');
+var merge       = require('merge-stream');
 var ngAnnotate  = require('gulp-ng-annotate');
 var preprocess  = require('gulp-preprocess');
+var spawn       = require('child_process').spawn;
 var uglify      = require('gulp-uglifyjs');
 
 // @todo add sizing as separate task
 // @todo update scss generation
-// @todo should pipe app-templates into app-scripts
+// @todo should handle app-templates and app-script better
 
 var dist_dir = './www/';
 
@@ -42,9 +44,9 @@ var paths = {
 };
 
 gulp.task('default', ['dev']);
-gulp.task('dev', ['app-fonts', 'app-assets', 'app-styles', 'lint', 'app-html', 'app-templates', 'lib-scripts', 'app-scripts-dev']);
-gulp.task('stg', ['app-fonts', 'app-assets', 'app-styles', 'lint', 'app-html', 'app-templates', 'lib-scripts', 'app-scripts-stg']);
-gulp.task('prd', ['app-fonts', 'app-assets', 'app-styles', 'lint', 'app-html', 'app-templates', 'lib-scripts', 'app-scripts-prd']);
+gulp.task('dev', ['app-fonts', 'app-assets', 'app-styles', 'lint', 'app-html', 'lib-scripts', 'app-scripts-dev']);
+gulp.task('stg', ['app-fonts', 'app-assets', 'app-styles', 'lint', 'app-html', 'lib-scripts', 'app-scripts-stg']);
+gulp.task('prd', ['app-fonts', 'app-assets', 'app-styles', 'lint', 'app-html', 'lib-scripts', 'app-scripts-prd']);
 
 gulp.task('app-fonts', function() {
   return gulp.src( paths.fonts )
@@ -81,7 +83,19 @@ gulp.task('lint', function() {
 
 app_scripts = function( env ) {
   return function() {
-    return gulp.src( paths.js )
+    var templates_js = gulp.src( paths.jade_tpl )
+      .pipe( jade({
+        locals: {},
+        pretty: false,
+      }))
+      .pipe( gulp.dest('./www/') )
+      .pipe( html2js({
+        moduleName: 'app-templates',
+        prefix: '',
+      }))
+      ;
+
+    return merge( templates_js, gulp.src( paths.js ) )
       .pipe( preprocess({ context: { NODE_ENV: env } }) )
       .pipe( concat('app.js') )
       .pipe( gulp.dest( dist_dir + 'js/' ) )
@@ -102,32 +116,13 @@ gulp.task('lib-scripts', function() {
   return gulp.src( paths.libs.js )
     .pipe( concat('libs.js') )
     .pipe( gulp.dest( dist_dir + 'js/' ) )
-    .pipe( ngAnnotate() )
-    .pipe( uglify( 'libs.min.js', {
-      outSourceMap: true,
-    }))
-    .pipe( gulp.dest( dist_dir + 'js/' ) )
     ;
 });
 
-gulp.task('app-templates', function() {
-  return gulp.src( paths.jade_tpl )
-    .pipe( jade({
-      locals: {},
-      pretty: false,
-    }))
-    .pipe( gulp.dest('./www/') )
-    .pipe( html2js({
-        moduleName: 'app-templates',
-        prefix: '',
-    }))
-    .pipe( concat('templates.js' ) )
-    .pipe( ngAnnotate() )
-    .pipe( gulp.dest( './www/js' ) )
-    .pipe( uglify( 'templates.min.js', {
-      outSourceMap: true,
-    }))
-    .pipe( gulp.dest('./www/js') )
+gulp.task('lib-scripts-min', function() {
+  return gulp.src( paths.libs.js_min )
+    .pipe( concat('libs.min.js') )
+    .pipe( gulp.dest( dist_dir + 'js/' ) )
     ;
 });
 
@@ -142,6 +137,7 @@ gulp.task('app-html', function() {
 });
 
 gulp.task('watch', function() {
+  gulp.watch( paths.gulp, ['reload'] );
   gulp.watch( paths.js, ['lint', 'app-scripts-dev'] );
   gulp.watch( paths.sass, ['app-styles'] );
   gulp.watch( paths.jade, ['app-html'] );
@@ -166,4 +162,21 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+});
+
+// http://stackoverflow.com/questions/22886682/how-can-gulp-be-restarted-on-gulpfile-change
+gulp.task('reload', function() {
+  var process;
+  var spawnChildren = function() {
+    // kill previous spawned process
+    if( process ) {
+      process.kill();
+    }
+
+    // `spawn` a child `gulp` process linked to the parent `stdio`
+    process = spawn( 'gulp', [], { stdio: 'inherit' } );
+  };
+
+  gulp.watch( ['./gulpfile.js'], spawnChildren );
+  spawnChildren();
 });
